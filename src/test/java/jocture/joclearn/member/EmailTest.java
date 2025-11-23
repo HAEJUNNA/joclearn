591b304a52fe09b91b1f436ee7503ee4e0cb5e2c
yamsroun <yamsroun@gmail.com>
2025-11-23 21:32:41 +0900

    2.4. 회원 도메인 모델 작성(5) - VO 도입

---
+++ src/test/java/jocture/joclearn/member/EmailTest.java
@@ -0,0 +1,27 @@
+package jocture.joclearn.member;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class EmailTest {
+
+    @Test
+    void create() {
+        Email email = new Email("jocture@gmail.com");
+        assertThat(email.value()).isEqualTo("jocture@gmail.com");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "jocture", "@abc.com", "j@a.c"
+    })
+    void create_fail(String value) {
+        assertThatThrownBy(() -> new Email(value))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+}
---
+++ src/test/java/jocture/joclearn/member/NicknameTest.java
@@ -0,0 +1,28 @@
+package jocture.joclearn.member;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+class NicknameTest {
+
+    @Test
+    void create() {
+        Nickname nickname = new Nickname("jocture");
+        assertThat(nickname.value()).isEqualTo("jocture");
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {
+        "JJ", "Lim", "123456789012345678901",
+        ".jocture", "_jocture", "-jocture",
+        "jocture*", "joc ture"
+    })
+    void create_fail(String value) {
+        assertThatThrownBy(() -> new Nickname(value))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+}
